97ef2d4af3744d8f935d1e59b888c954b7fb0c6d
Ishaan Ahuja <ishaanahuja0@gmail.com>
2022-05-10 09:22:41 +0000

    Resolve "p-Pb analysis"

---
+++ analysis/p-Pb/AddMyTask.C
@@ -0,0 +1,47 @@
+AliCascadeAnalysis_Ishaan *AddMyTask(bool K0s = false, bool Lam = false, bool Xi = true, bool Om = true, TString suffix = "")
+{
+  // analysis manager
+  AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+  if (!mgr)
+  {
+    ::Error("AddMyTask", "No analysis manager to connect to.");
+    return NULL;
+  }
+  if (!mgr->GetInputEventHandler())
+  {
+    ::Error("AddMyTask", "This task requires an input event handler");
+    return NULL;
+  }
+
+  // Create the task and add it to the manager
+  TString tskname = Form("Cascade_analysis%s", suffix.Data());
+  AliCascadeAnalysis_Ishaan *mytask = new AliCascadeAnalysis_Ishaan(tskname);
+  mgr->AddTask(mytask);
+  mytask->SetParticleAnalysisStatus(K0s, Lam, Xi, Om);
+
+  // output file name
+  TString outputFileName = AliAnalysisManager::GetCommonFileName();
+  outputFileName += ":Cascade_analysis";
+  printf("Set OutputFileName : \n %s\n", outputFileName.Data());
+
+  //create and link only used containers
+  AliAnalysisDataContainer *coutput[8];
+  TString clabels[8] = {"eve", "K0S", "Lam", "ALam", "Xim", "Xip", "Omm", "Omp"};
+
+  mgr->ConnectInput(mytask, 0, mgr->GetCommonInputContainer());
+  coutput[0] = mgr->CreateContainer(Form("chists_%s_%s", clabels[0].Data(), suffix.Data()), TList::Class(), AliAnalysisManager::kOutputContainer, outputFileName);
+  mgr->ConnectOutput(mytask, 1, coutput[0]);
+
+  int cnumber = 1;
+  for (int icont = 1; icont < 8; icont++)
+  {
+    if (mytask->GetParticleAnalysisStatus(icont - 1))
+    {
+      coutput[cnumber] = mgr->CreateContainer(Form("chists_%s_%s", clabels[icont].Data(), suffix.Data()), TList::Class(), AliAnalysisManager::kOutputContainer, outputFileName);
+      mgr->ConnectOutput(mytask, cnumber + 1, coutput[cnumber]);
+      cnumber++;
+    }
+  }
+
+  return mytask;
+}
---
+++ analysis/p-Pb/README.md
@@ -0,0 +1,29 @@
+# QuickStart
+
+## Running Locally
+
+Clone project and navigate to this folder.
+
+1. Start `cvmfs`
+```shell
+systemctl start cvmfs
+```
+
+2. Enter CentOS 7 toolbox
+```shell
+toolbox enter c7
+```
+
+3. Login to AliROOT and setup ALICE environment
+```shell
+source /cvmfs/alice.cern.ch/etc/login.sh && alienv enter VO_ALICE@AliPhysics::vAN-20210223_ROOT6-1
+```
+4.  Run analysis locally
+```shell
+root runAnalysis_ROOT6.C
+```
+The results are stored in `AnalysisResults.root`
+
+
+### Note
+1. For more info on running analysis tasks, please see [here](https://gitlab.science.upjs.sk/jf/support/-/blob/master/doc/ALICE/README.md).
